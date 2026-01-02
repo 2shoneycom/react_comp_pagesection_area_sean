2c0c8c7065e81550923eb53e4cdfde6668567b0b
2shoneycom <2shoneycom@gmail.com>
2026-01-03 01:10:49 +0900

    Carousel Slider Logic (1)
    Work On:
    -Complete Button Action Logic
    
    To Do:
    -Drag Action Logic

--- src/Section_typeA.js
+++ src/Section_typeA.js
@@ -3,27 +3,22 @@ import styles from "./css/section_typeA.module.css";
 import { useState, useEffect } from "react";
 
 function Section_typeA({ data = [] }) {
-  // todo: 변수 설정 -> 현재 페이지 인덱스 (초기 값 1, 최대 slideCount)
-  // 현재 페이지 인덱스가 1이면 버튼은 prev 버튼 비활성화
-  // 현재 페이지 인덱스가 최대 slideCount면 next 버튼 비활성화
-  // next 버튼 누르면 현재 페이지 인덱스 +1 되면서 동시에 슬라이드 이동
-  // prev 버튼 누르면 현재 페이지 인덱스 -1 되면서 동시에 슬라이드 이동
+  // TO DO
+  // 1. 지금은 버튼만으로 슬라이드 이동이 가능한데, 드래그로도 가능하게 만들어야 함.
+  // 2. 반응형 웹 디자인
 
   // 받은 data 전처리 (2개씩 그룹핑)
-  const groups = [];
+  const groups = [];  // groups = [ [data1, data2], [data3, data4], ... ]
   for (let i = 0; i < data.length; i += 2) {
     groups.push(data.slice(i, i + 2));
   }
-  // groups = [ [data1, data2], [data3, data4], ... ]
 
-
-  // 기본값은 데스크톱 기준(2)으로 시작
+  // ---------------------------------------------------------
+  // 1. 화면 크기에 따라 보여지는 개수 계산 로직 
+  // ---------------------------------------------------------
   const [itemsPerSlide, setItemsPerSlide] = useState(2);
   const slideCount = Math.ceil(groups.length / itemsPerSlide);
 
-  // ---------------------------------------------------------
-  // 2. 화면 크기에 따라 보여지는 개수 계산 로직 
-  // ---------------------------------------------------------
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;  // 현재 브라우저의 너비
@@ -43,6 +38,16 @@ function Section_typeA({ data = [] }) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  // ---------------------------------------------------------
+  // 2. 슬라이드 이동 효과 구현
+  // ---------------------------------------------------------
+  const [curSlide, SetCurSlide] = useState(0);
+  const [transAmount, SetTransAmount] = useState(0);
+
+  useEffect(() => {
+    SetTransAmount(curSlide * (-101.31));
+  }, [curSlide]);
+
   return (
     <div className={styles.top_container}>
       {/* 1. Title Area */}
@@ -53,7 +58,10 @@ function Section_typeA({ data = [] }) {
       {/* 2. Carousel Slider Area */}
       <div className={styles.carousel_slider}>
         <div className={styles.flickity_viewport}>
-          <div className={styles.flickity_slider}>
+          <div
+            className={styles.flickity_slider}
+            style={{ transform: `translateX(${transAmount}%)` }}
+          >
 
             {groups.map((group, groupIdx) => (
               <div
@@ -64,9 +72,8 @@ function Section_typeA({ data = [] }) {
                   <div key={item.id} className={styles.ResearchAreaCard}>
                     <Link className={styles.ResearchAreaCard_link} to={item.link}>
                       <div className={styles.ResearchAreaCard_media}>
-                        <picture class="flickity-lazyloaded">
-                          <source width="48" height="48" data-image-size="mediumIcon" src="null" srcset={item.media_source} />
-                          <img class="Image" data-image-size="mediumIcon" alt={`${item.title}.svg`} width="48" height="48" src={item.media_source} />
+                        <picture>
+                          <img data-image-size="mediumIcon" alt={`${item.title}.svg`} width="48" height="48" src={item.media_source} />
                         </picture>
                       </div>
                       <div className={styles.ResearchAreaCard_content}>
@@ -81,25 +88,32 @@ function Section_typeA({ data = [] }) {
             ))}
           </div>
         </div>
-        <button className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.previous}`}>
-          <svg class={styles.flickity_button_icon} viewBox="0 0 100 100">
+        <button
+          className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.previous}`}
+          onClick={() => SetCurSlide(curSlide - 1)}
+          style={curSlide === 0 ? { display: 'none' } : null}
+        >
+          <svg className={styles.flickity_button_icon} viewBox="0 0 100 100">
             <title>Previous</title>
             <path d="M0.6,0.3c-0.4,0.4-0.4,1,0,1.4L5.8,7l-5.2,5.2c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l6.5-6.5L8.7,7L8.5,6.8 L2,0.3C1.6-0.1,1-0.1,0.6,0.3z" transform="translate(100, 100) rotate(180)"></path>
           </svg>
         </button>
-        <button className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.next}`}>
-          <svg class={styles.flickity_button_icon} viewBox="0 0 100 100">
+        <button
+          className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.next}`}
+          onClick={() => SetCurSlide(curSlide + 1)}
+          style={curSlide === slideCount-1 ? { display: 'none' } : null}
+        >
+          <svg className={styles.flickity_button_icon} viewBox="0 0 100 100">
             <title>Next</title>
             <path d="M0.6,0.3c-0.4,0.4-0.4,1,0,1.4L5.8,7l-5.2,5.2c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l6.5-6.5L8.7,7L8.5,6.8 L2,0.3C1.6-0.1,1-0.1,0.6,0.3z" transform="translate(100, 100) rotate(180)"></path>
           </svg>
         </button>
         <div className={styles.flickity_page_dots}>
           {Array.from({ length: slideCount }).map((_, index) => (
-            <button 
-              key={index} 
-              className={styles.flickity_page_dot} // CSS에 .dot 스타일 추가 필요
-              // 클릭 시 해당 페이지로 이동하는 기능은 나중에 추가
-              onClick={() => console.log(`${index}번 페이지로 이동`)} 
+            <button
+              key={index}
+              className={`${styles.flickity_page_dot}  ${index === curSlide ? styles.is_selected : ""}`}
+              onClick={() => SetCurSlide(index)}
             ></button>
           ))}
         </div>
