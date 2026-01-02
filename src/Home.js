f0cfa5ac6b15e6f4d001e11444bcc17c3d2da369
2shoneycom <2shoneycom@gmail.com>
2026-01-02 23:48:44 +0900

    Layout Work
    To Do:
    -Carousel Slider Logic

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,93 @@
 import styles from "./css/home.module.css";
 import AreaSection from "./Section_typeA";
 
+const researchAreaDataSample = [
+  {
+    id: 1,
+    title: "Automated reasoning",
+    media_source: "https://assets.amazon.science/b3/e6/339a287646308dd0cf27be64860a/automatedreasoning.svg",
+    link: "area/1",
+  },
+  {
+    id: 2,
+    title: "Cloud and systems",
+    media_source: "https://assets.amazon.science/94/b0/94affd2444f7a0d80227106f9211/cloud.svg",
+    link: "area/2",
+  },
+  {
+    id: 3,
+    title: "Computer vision",
+    media_source: "https://assets.amazon.science/9c/02/b5c9fb95439ebf5d4bcaef6fc4cd/computervision.svg",
+    link: "area/3",
+  },
+  {
+    id: 4,
+    title: "Conversational AI",
+    media_source: "https://assets.amazon.science/fe/aa/910fd701416dabb6d3316fe2ec19/conversationalai.svg",
+    link: "area/4",
+  },
+  {
+    id: 5,
+    title: "Economics",
+    media_source: "https://assets.amazon.science/a9/bc/ab88878c4faebca412cbef3b022b/economics.svg",
+    link: "area/5",
+  },
+  {
+    id: 6,
+    title: "Information and knowledge management",
+    media_source: "https://assets.amazon.science/4d/25/9ac984a8444e80d8071730edb6a0/infomanagement.svg",
+    link: "area/6",
+  },
+  {
+    id: 7,
+    title: "Machine learning",
+    media_source: "https://assets.amazon.science/93/06/f6c7a0ae49ec9d9427ce8dde796f/machinelearning.svg",
+    link: "area/7",
+  },
+  {
+    id: 8,
+    title: "Operations research and optimization",
+    media_source: "https://assets.amazon.science/65/a0/8603864b451fbce8ce61f0ca85e7/operations.svg",
+    link: "area/8",
+  },
+  {
+    id: 9,
+    title: "Quantum technologies",
+    media_source: "https://assets.amazon.science/18/56/44f7716948d58dc87d0d1c8ed3bc/quantum-technologies.svg",
+    link: "area/9",
+  },
+  {
+    id: 10,
+    title: "Robotics",
+    media_source: "https://assets.amazon.science/9f/d6/2a081f10407db7bc1a99f17be15d/robotics.svg",
+    link: "area/10",
+  },
+  {
+    id: 11,
+    title: "Search and information retrieval",
+    media_source: "https://assets.amazon.science/0f/8a/4bf8055f47499b18da29af6b95c4/search.svg",
+    link: "area/11",
+  },
+  {
+    id: 12,
+    title: "Security, privacy, and abuse prevention",
+    media_source: "https://assets.amazon.science/35/42/bdaf2a4c4e8d887ffcfdaf03ddee/security.svg",
+    link: "area/12",
+  },
+  {
+    id: 13,
+    title: "Sustainability",
+    media_source: "https://assets.amazon.science/09/1d/d2ef27534875b71e1919bac64520/sustainability.svg",
+    link: "area/13",
+  },
+];
+
 function Home() {
   return (
     <div className={styles.top_container}>
       <div className={styles.header_area}></div>
       <div className={styles.hero_area}></div>
-      <AreaSection />
+      <AreaSection data={researchAreaDataSample}/>
       <div className={styles.section1_area}></div>
       <div className={styles.section2_area}></div>
       <div className={styles.section1_area}></div>
--- src/Section_typeA.js
+++ src/Section_typeA.js
@@ -1,9 +1,109 @@
+import { Link } from "react-router-dom";
 import styles from "./css/section_typeA.module.css";
+import { useState, useEffect } from "react";
 
-function Section_typeA() {
+function Section_typeA({ data = [] }) {
+  // todo: 변수 설정 -> 현재 페이지 인덱스 (초기 값 1, 최대 slideCount)
+  // 현재 페이지 인덱스가 1이면 버튼은 prev 버튼 비활성화
+  // 현재 페이지 인덱스가 최대 slideCount면 next 버튼 비활성화
+  // next 버튼 누르면 현재 페이지 인덱스 +1 되면서 동시에 슬라이드 이동
+  // prev 버튼 누르면 현재 페이지 인덱스 -1 되면서 동시에 슬라이드 이동
+
+  // 받은 data 전처리 (2개씩 그룹핑)
+  const groups = [];
+  for (let i = 0; i < data.length; i += 2) {
+    groups.push(data.slice(i, i + 2));
+  }
+  // groups = [ [data1, data2], [data3, data4], ... ]
+
+
+  // 기본값은 데스크톱 기준(2)으로 시작
+  const [itemsPerSlide, setItemsPerSlide] = useState(2);
+  const slideCount = Math.ceil(groups.length / itemsPerSlide);
+
+  // ---------------------------------------------------------
+  // 2. 화면 크기에 따라 보여지는 개수 계산 로직 
+  // ---------------------------------------------------------
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;  // 현재 브라우저의 너비
+
+      if (width < 768) {
+        setItemsPerSlide(1);
+      } else {
+        setItemsPerSlide(2);
+      }
+    };
+
+    handleResize(); // 컴포넌트 처음 뜰 때 한 번 실행
+
+    window.addEventListener("resize", handleResize);  // 화면 크기 바뀔 때마다 실행되도록 리스너 부착
+
+    // 뒷정리 (Cleanup): 컴포넌트 사라질 때 리스너 제거
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className={top_container}>
-      
+    <div className={styles.top_container}>
+      {/* 1. Title Area */}
+      <div className={styles.ResearchArea_header_wrapper}>
+        <h2 className={styles.ResearchArea_header}>Research areas</h2>
+      </div>
+
+      {/* 2. Carousel Slider Area */}
+      <div className={styles.carousel_slider}>
+        <div className={styles.flickity_viewport}>
+          <div className={styles.flickity_slider}>
+
+            {groups.map((group, groupIdx) => (
+              <div
+                key={groupIdx}
+                className={styles.flickity_group}
+              >
+                {group.map((item) => (
+                  <div key={item.id} className={styles.ResearchAreaCard}>
+                    <Link className={styles.ResearchAreaCard_link} to={item.link}>
+                      <div className={styles.ResearchAreaCard_media}>
+                        <picture class="flickity-lazyloaded">
+                          <source width="48" height="48" data-image-size="mediumIcon" src="null" srcset={item.media_source} />
+                          <img class="Image" data-image-size="mediumIcon" alt={`${item.title}.svg`} width="48" height="48" src={item.media_source} />
+                        </picture>
+                      </div>
+                      <div className={styles.ResearchAreaCard_content}>
+                        <div className={styles.ResearchAreaCard_title}>
+                          {item.title}
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.previous}`}>
+          <svg class={styles.flickity_button_icon} viewBox="0 0 100 100">
+            <title>Previous</title>
+            <path d="M0.6,0.3c-0.4,0.4-0.4,1,0,1.4L5.8,7l-5.2,5.2c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l6.5-6.5L8.7,7L8.5,6.8 L2,0.3C1.6-0.1,1-0.1,0.6,0.3z" transform="translate(100, 100) rotate(180)"></path>
+          </svg>
+        </button>
+        <button className={`${styles.flickity_button} ${styles.flickity_prev_next_button} ${styles.next}`}>
+          <svg class={styles.flickity_button_icon} viewBox="0 0 100 100">
+            <title>Next</title>
+            <path d="M0.6,0.3c-0.4,0.4-0.4,1,0,1.4L5.8,7l-5.2,5.2c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l6.5-6.5L8.7,7L8.5,6.8 L2,0.3C1.6-0.1,1-0.1,0.6,0.3z" transform="translate(100, 100) rotate(180)"></path>
+          </svg>
+        </button>
+        <div className={styles.flickity_page_dots}>
+          {Array.from({ length: slideCount }).map((_, index) => (
+            <button 
+              key={index} 
+              className={styles.flickity_page_dot} // CSS에 .dot 스타일 추가 필요
+              // 클릭 시 해당 페이지로 이동하는 기능은 나중에 추가
+              onClick={() => console.log(`${index}번 페이지로 이동`)} 
+            ></button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
